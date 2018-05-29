5b9a4b18072dc9b72568baae2130ce454f3a74a2
aidmas <37577938+aidmas@users.noreply.github.com>
2018-05-29 22:45:18 +0300

    Miau

---
+++ Lab1_BazinesStrukturos/src/BandymuLaboratorija.java
@@ -0,0 +1,42 @@
+/**
+ * Tai pradinė tyrimų klasė, iš kurios pasirinktinai yra nukreipiama
+ * į Javos elementų savybes demonstruojančius metodus.
+ * Jie naudingi konkrečių savybių pažinimui sustiprinti.
+ *
+ * Kadangi visi metodai su System.out.println pateikia nemažai informacijos,
+ * todėl komentarų pagalba rekomenduojama išskirti tik vieną tiriamą klasę ir
+ * metodus, o kitus padaryti neaktyviais ir koncentruotis į vieną problemą.
+ *
+ * Daugelis metodų yra "vienkartinio" naudojimo ir algoritmų ieškoti nereikia.
+ * Jei kokie nors aspektai yra neaiškūs, tai daromi metodo pakeitimai ir
+ * tyrinėjama toliau, stebint gautus rezultatus.
+ *
+ * Pateiktos klasės gali būti naudojamos kaip šabloninių sprendimų saugyklos.
+ * Toliau kylant naujoms problemoms, papildomai sukuriamos naujos klasės
+ * ar metodai, jie išbandomi ir tada perkeliami į kuriamą programą.
+ *
+ * DAUG KLAUSIMŲ APIE JAVĄ GALIMA IŠSIAIŠKINTI, ATLIEKANT
+ * SAVARANKIŠKUS BANDYMUS, TYRIMUS IR ANALIZĘ :)
+ *
+ * Užduotis: Peržiūrėkite ir išsiaiškinkite pateiktas klases,
+ * pakeiskite programinį kodą, stebėkite gautus rezultatus.
+ */
+public class BandymuLaboratorija {
+    /**
+     * Reikalingoms demonstravimui klasėms komentarai nuimami.
+     * Laikinai nereikalingoms komentarai uždedami.
+     */
+    public static void main(String[] args) {
+        Demo0PradiniaiEtiudai.metodųIšbandymas();
+//        Demo1SkaiciuSavybes  .metodoParinkimas();
+//        Demo2SimboliaiTekstai.metodoParinkimas();
+//        new Demo3MasyvaiPerziura().metodoParinkimas();
+//
+//        Demo4NuosaviObjektai demoObjektas= new Demo4NuosaviObjektai();
+//        demoObjektas.metodoParinkimas();
+//
+//        Demo5LokalesLietuvybe.metodoParinkimas();
+//        Demo6LaikasDatos     .metodoParinkimas();
+//        PirmoKursoUzdavinys.metodųIšbandymas();
+    }
+}
